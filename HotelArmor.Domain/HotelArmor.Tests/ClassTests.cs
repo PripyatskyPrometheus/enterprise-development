47dd3a09fe020710e00512197e8b5da4b460474a
Chelaev Petr <petrcelaev@gmail.com>
2024-10-13 21:02:45 +0400

    Ещё одна попытка

--- HotelArmor.Domain/HotelArmor.Tests/ClassTests.cs
+++ HotelArmor.Domain/HotelArmor.Tests/ClassTests.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Runtime.InteropServices;
 using Xunit.Sdk;
-
+using FactAttribute;
 namespace HotelArmor.Tests;
 
 public class ClassTests (HotelArmorData data) : IClassFixture<HotelBookingDetailsData> {
