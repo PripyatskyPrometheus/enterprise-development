3776e24888d3956b504a6d53a1c35cd0b4dea84f
Chelaev Petr <petrcelaev@gmail.com>
2024-10-15 13:40:19 +0400

    Исправил недочёты в тестах и внесении данных

--- HotelArmor.Domain/HotelArmor.Tests/ClassTests.cs
+++ HotelArmor.Domain/HotelArmor.Tests/ClassTests.cs
@@ -1,30 +1,41 @@
 ﻿using HotelArmor.Domain;
-using System.Linq;
-using System.Runtime.InteropServices;
-
 namespace HotelArmor.Tests;
 
-public class ClassTests (HotelArmorData data) : IClassFixture<HotelArmorData> {
+/// <summary>
+/// Класс с шестью тестами
+/// </summary>
+public class Test(HotelArmorData data) : IClassFixture<HotelArmorData> {
     private readonly HotelArmorData data_a = data;
 
+    /// <summary>
+    /// Тест на вывод сведений о всех отелях
+    /// </summary>
     [Fact]
     public void ReturnAllHotels() {
-        var countHotels = data_a.Hotels.Select(h => h.Name).ToList();
-        var expectedNumber = 3;
-        Assert.Equal(expectedNumber, countHotels.Count);
+        var countHotels = data_a.Hotels.Select(h => h).ToList();
+        var expectedHotels = new List<Hotel> {
+            data_a.Hotels[0],
+            data_a.Hotels[1],
+            data_a.Hotels[2],
+            data_a.Hotels[3],
+            data_a.Hotels[4],
+            data_a.Hotels[5]
+        };
+        Assert.Equal(expectedHotels, countHotels);
     }
 
+    /// <summary>
+    /// Тест на вывод клиентов, проживающих в указанном отеле
+    /// </summary>
     [Fact]
     public void ReturnAllClientInHotel() {
         var expecteClients = new List<Client> {
-            data_a.Clients[0],
-            data_a.Clients[1],
-            data_a.Clients[2],
-            data_a.Clients[3],
-            data_a.Clients[4],
+            data_a.Clients[9],
+            data_a.Clients[10],
+            data_a.Clients[8],
+            data_a.Clients[11]
         };
-
-        var hotelId = data_a.Hotels.Where(h => h.Name == "Россия").Select(h => h.ID).First();
+        var hotelId = data_a.Hotels.Where(h => h.Name == "Hilton").Select(h => h.ID).First();
         var clientInHotel = data_a.ArmoredRooms
             .OrderBy(r => r.Client.FullName)
             .Where(r => data_a.Rooms
@@ -35,6 +46,9 @@ public class ClassTests (HotelArmorData data) : IClassFixture<HotelArmorData> {
         Assert.Equal(clientInHotel, expecteClients);
     }
 
+    /// <summary>
+    /// Тест на вывод топа 5 отелей по числу бронирования
+    /// </summary>
     [Fact]
     public void ReturnTopFiveHotel() {
         var expecteHhotels = new List<Hotel> {
@@ -44,7 +58,6 @@ public class ClassTests (HotelArmorData data) : IClassFixture<HotelArmorData> {
             data_a.Hotels[3],
             data_a.Hotels[5]
         };
-
         var topFiveHotel = data_a.ArmoredRooms
             .GroupBy(r => r.Room.HotelID)
             .Select(r => r.Key)
@@ -56,39 +69,40 @@ public class ClassTests (HotelArmorData data) : IClassFixture<HotelArmorData> {
             .OrderBy(r => r.ID)
             .ToList();
         Assert.Equal(expecteHhotels, topFiveHotel);
-
     }
 
+    /// <summary>
+    /// Тест на вывод информации о свободных номерах, в указанном городе
+    /// </summary>
     [Fact]
     public void ReturnFreeRooms() {
-        var expectedRooms = new List<Room> {
-            data_a.Rooms[0],
-            data_a.Rooms[2],
-            data_a.Rooms[4],
+        var expectedRooms = new List<Room> { 
+            data_a.Rooms[4] 
         };
+        var city = "New York";
+        var bookedRoomIds = data_a.ArmoredRooms
+           .Where(r => r.DateEvection == DateOnly.ParseExact("0001-01-01", "yyyy-mm-dd"))
+           .Select(r => r.Room.ID)
+           .ToList();
 
-        var city = "Москва";
-        var freeRooms = data_a.ArmoredRooms
-            .Where(r => !data_a.ArmoredRooms.Where(r => r.DateEvection == DateOnly.ParseExact("0001-01-01", "yyyy-mm-dd"))
-                .Select(r => r).ToList().Contains(r)
-                &&
-                data_a.Rooms.Where(r => (data_a.Hotels.Where(h => h.City == city).Select(h => h.ID).ToList()).Contains(r.HotelID))
-                .Select(h => h).ToList().Contains(r.Room)
-                ).Select(r => r.Room).ToList();
+        var freeRooms = data_a.Rooms
+            .Where(r => bookedRoomIds.Contains(r.ID)
+                && data_a.Hotels.Any(h => h.City == city && h.ID == r.HotelID)
+            )
+            .ToList();
+
         Assert.Equal(expectedRooms, freeRooms);
     }
 
+    /// <summary>
+    /// Тест на возврат сведений о клиентах, занявших номера на самое продолжительное время
+    /// </summary>
     [Fact]
     public void ReturnLongLiversHotel() {
-
         var expecteClients = new List<Client> {
-            data_a.Clients[0],
-            data_a.Clients[1],
-            data_a.Clients[2],
-            data_a.Clients[3],
-            data_a.Clients[4]
+            data_a.Clients[4],
+            data_a.Clients[13]
         };
-
         var longerPeriods = data_a.ArmoredRooms
             .GroupBy(c => c.Client)
             .Select(c => new {
@@ -102,23 +116,24 @@ public class ClassTests (HotelArmorData data) : IClassFixture<HotelArmorData> {
                 Client = c.Key,
                 Total = c.Sum(r => r.Period)
             }).Where(c => c.Total == longerPeriods).Select(c => c.Client).ToList();
-
         Assert.Equal(expecteClients, clientWithLongerPer);
     }
 
+    /// <summary>
+    /// Тест на вывод сведений о минимальной, максимальной и средней ценах за комнаты в каждом отеле
+    /// </summary>
     [Fact]
     public void MinAvgMaxCostInHotel() {
         var hotels = data_a.Hotels.Select(h => h);
-         
+
         var hotelCosts = hotels.Select(h => new {
             Hotel = (data_a.Hotels.Where(hotel => hotel.ID == h.ID).Select(hotel => hotel)),
             Min = data_a.Rooms.Where(r => r.HotelID == h.ID).Select(r => r).ToList().Min(rm => rm.Cost),
             Max = data_a.Rooms.Where(r => r.HotelID == h.ID).Select(r => r).ToList().Max(rm => rm.Cost),
             Avg = data_a.Rooms.Where(r => r.HotelID == h.ID).Select(r => r).ToList().Average(rm => rm.Cost)
         }).ToList();
-
         Assert.Equal(3000, hotelCosts[0].Min);
-        Assert.Equal(4500.00, hotelCosts[0].Avg, 2);
+        Assert.Equal(4500, hotelCosts[0].Avg);
         Assert.Equal(6000, hotelCosts[0].Max);
     }
 }
--- HotelArmor.Domain/HotelArmor.Tests/HotelArmorData.cs
+++ HotelArmor.Domain/HotelArmor.Tests/HotelArmorData.cs
@@ -4,160 +4,214 @@ namespace HotelArmor.Tests;
 public class HotelArmorData {
 
     public List<TypeRoom> TypesRoom = [
-       new(){ ID = 0, NameRoom = "Luxury"},
-       new(){ID = 1, NameRoom ="Economy"},
-       new(){ID = 2, NameRoom ="Comfort"}
-        ];
+        new(){ ID = 1,  NameRoom = "Luxary"},
+        new(){ ID = 2,  NameRoom = "Economy"},
+        new(){ ID = 3,  NameRoom = "Comfort"}
+    ];
 
     public List<Pasport> Pasports = [
-        new(){ID = 0, Series = 6112, Number = 100503},
-        new(){ID = 1, Series = 6212, Number = 305610},
-        new(){ID = 2, Series = 6312, Number = 678390},
-        new(){ID = 3, Series = 6412, Number = 875567},
-        new(){ID = 4, Series = 6512, Number = 903490}
+        new(){ID = 1, Series = 6313, Number = 123421},
+        new(){ID = 2, Series = 1234, Number = 567890 },
+        new(){ID = 3, Series = 5678, Number = 987654},
+        new(){ID = 4, Series = 7890, Number = 234567},
+        new(){ID = 5, Series = 3456, Number = 876543},
+        new(){ID = 6, Series = 9012, Number = 345678},
+        new(){ID = 7, Series = 2345, Number = 765432},
+        new(){ID = 8, Series = 6789, Number = 654321},
+        new(){ID = 9, Series = 4321, Number = 123456},
+        new(){ID = 10, Series = 0987, Number = 456789},
+        new(){ID = 11, Series = 8765, Number = 789012 },
+        new(){ID = 12, Series = 6543, Number = 345678},
+        new(){ID = 13, Series = 2109, Number = 890123},
+        new(){ID = 14, Series = 8765, Number = 456789},
+        new(){ID = 15, Series = 5432, Number = 321098},
+        new(){ID = 16, Series = 6789, Number = 901234},
+        new(){ID = 17, Series = 1234, Number = 345678},
+        new(){ID = 18, Series = 9876, Number = 567890},
+        new(){ID = 19, Series = 3456, Number = 789012},
+        new(){ID = 20, Series = 6789, Number = 890123},
         ];
 
     public List<Room> Rooms;
-    public List<Client> Clients;
     public List<Hotel> Hotels;
+    public List<Client> Clients;
     public List<ArmoredRoom> ArmoredRooms;
 
     public HotelArmorData() {
         Clients = [
-            new(){
-                ID = 0,
-                FullName = "Жмуриков Анатолий Степанович",
+            new() {
+                ID = 1,
+                FullName = "Иванов Иван Иванович",
                 PasportData = Pasports[0],
-                BirthOfDay = new DateOnly(1986, 12, 30)
+                BirthOfDay =  new DateOnly(2003, 1, 1)
             },
-            new(){
-                ID = 1,
-                FullName = "Морозов Сергей Владиславович",
+            new() {
+                ID = 2,
+                FullName = "Петров Петр Петрович",
                 PasportData = Pasports[1],
-                BirthOfDay = new DateOnly(1972, 5, 12)
+                BirthOfDay =  new DateOnly(2002, 5, 15 )
             },
-            new(){
-                ID = 2,
-                FullName = "Османов Мехмед Ибрагимович",
-                PasportData = Pasports[2],
-                BirthOfDay = new DateOnly(1990, 4, 7)
-            },
-            new(){
+            new() {
                 ID = 3,
-                FullName = "Уханов Никита Олегович",
-                PasportData = Pasports[3],
-                BirthOfDay = new DateOnly(1989, 10, 17)
+                FullName = "Сидоров Сидор Сидорович",
+                PasportData = Pasports[2],
+                BirthOfDay =  new DateOnly(2001, 9, 20 )
             },
-            new(){
+            new() {
                 ID = 4,
-                FullName = "Андреев Данила Валерьевич",
+                FullName = "Кузнецов Алексей Владимирович",
+                PasportData = Pasports[3],
+                BirthOfDay =  new DateOnly(2004, 3, 10)
+            },
+            new() {
+                ID = 5,
+                FullName = "Смирнов Дмитрий Александрович",
                 PasportData = Pasports[4],
-                BirthOfDay = new DateOnly(2000, 7, 18)
-            }
-            ];
-
-        Hotels = [
-            new() {
-                ID = 0,
-                Name = "Россия",
-                Address = "Ленина 32",
-                City = "Санкт-Пеербург"
+                BirthOfDay =  new DateOnly(2000, 8, 27)
             },
             new() {
-                ID = 1,
-                Name = "Москва",
-                Address = "Лукачёва 65",
-                City = "Самара"
+                ID = 6,
+                FullName = "Козлов Игорь Сергеевич",
+                PasportData = Pasports[5],
+                BirthOfDay =  new DateOnly(2005, 2, 18)
             },
             new() {
-                ID = 2,
-                Name = "Лаза",
-                Address = "Гагарин 55",
-                City = "Новосибирск"
+                ID = 7,
+                FullName = "Лебедев Владислав Денисович",
+                PasportData = Pasports[6],
+                BirthOfDay =  new DateOnly(2003, 10, 05)
+            },
+            new() {
+                ID = 8,
+                FullName = "Никитин Андрей Алексеевич",
+                PasportData = Pasports[7],
+                BirthOfDay =  new DateOnly(2001, 12, 30)
+            },
+            new() {
+                ID = 9,
+                FullName = "Морозов Кирилл Васильевич",
+                PasportData = Pasports[8],
+                BirthOfDay =  new DateOnly(2000, 4, 09)
+            },
+            new() {
+                ID = 10,
+                FullName = "Андреев Евгений Дмитриевич",
+                PasportData = Pasports[9],
+               BirthOfDay =  new DateOnly(2002, 7, 14)
+            },
+            new() {
+                ID = 11,
+                FullName = "Богданова Елена Игоревна",
+                PasportData = Pasports[10],
+                BirthOfDay =  new DateOnly(2004, 11, 21)
+            },
+            new() {
+                ID = 12,
+                FullName = "Семенов Максим Павлович",
+                PasportData = Pasports[11],
+                BirthOfDay =  new DateOnly(2003, 6, 2)
+            },
+            new() {
+                ID = 13,
+                FullName = "Козлова Виктория Алексеевна",
+                PasportData = Pasports[12],
+                BirthOfDay =  new DateOnly(2000, 12, 15)
+            },
+            new() {
+                ID = 14,
+                FullName = "Новикова Анастасия Сергеевна",
+                PasportData = Pasports[13],
+                BirthOfDay =  new DateOnly(2001, 3, 25)
+            },
+            new() {
+                ID = 15,
+                FullName = "Гаврилова Ольга Владимировна",
+                PasportData = Pasports[14],
+                BirthOfDay =  new DateOnly(2002, 9, 12)
+            },
+            new() {
+                ID = 16,
+                FullName = "Белякова Светлана Ивановна",
+                PasportData = Pasports[15],
+                BirthOfDay =  new DateOnly(2004, 7, 3)
+            },
+            new() {
+                ID = 17,
+                FullName = "Федорова Екатерина Дмитриевна",
+                PasportData = Pasports[16],
+                BirthOfDay =  new DateOnly(2003, 4, 28)
+            },
+            new() {
+                ID = 18,
+                FullName = "Алексеева Ирина Александровна",
+                PasportData = Pasports[17],
+                BirthOfDay =  new DateOnly(2001, 10, 17)
+            },
+            new() {
+                ID = 19,
+                FullName = "Тихонова Татьяна Васильевна",
+                PasportData = Pasports[18],
+                BirthOfDay =  new DateOnly(2001, 10, 17)
+            },
+            new()
+            {
+                ID = 20,
+                FullName = "Жмурова Мелания Максимова",
+                PasportData = Pasports[19],
+                BirthOfDay =  new DateOnly(2002, 02, 17)
             }
         ];
 
         Rooms = [
-            new() {
-                ID = 0, 
-                Type = TypesRoom[0],
-                Capacity = 2, 
-                Cost = 5000, 
-                HotelID = 0
-            },
-            new() {
-                ID = 1,
-                Type = TypesRoom[1],
-                Capacity = 2,
-                Cost = 1000,
-                HotelID = 1
-            },
-            new() {
-                ID = 2,
-                Type = TypesRoom[2],
-                Capacity = 3,
-                Cost = 3000,
-                HotelID = 2
-            },
-            new() {
-                ID = 3,
-                Type = TypesRoom[0],
-                Capacity = 3,
-                Cost = 5000,
-                HotelID = 1
-            },
-            new() {
-                ID = 4,
-                Type = TypesRoom[1],
-                Capacity = 3,
-                Cost = 1200,
-                HotelID = 2
-            },
-            new() {
-                ID = 5,
-                Type = TypesRoom[2],
-                Capacity = 5,
-                Cost = 3500,
-                HotelID = 0
-            }
+            new(){ID = 1, Capacity = 2, Cost = 3000, Type = TypesRoom[0], HotelID = 0},
+            new(){ID = 2, Capacity = 1, Cost = 4000, Type = TypesRoom[1], HotelID = 0},
+            new(){ID = 3, Capacity = 3, Cost = 5000, Type = TypesRoom[2], HotelID = 0},
+            new(){ID = 4, Capacity = 2, Cost = 6000, Type = TypesRoom[0], HotelID = 0},
+            new(){ID = 5, Capacity = 1, Cost = 7000, Type = TypesRoom[1], HotelID = 1},
+            new(){ID = 6, Capacity = 5, Cost = 8000, Type = TypesRoom[2], HotelID = 1},
+            new(){ID = 7, Capacity = 3, Cost = 9000, Type = TypesRoom[0], HotelID = 1},
+            new(){ID = 8, Capacity = 2, Cost = 10000, Type = TypesRoom[1], HotelID = 1},
+            new(){ID = 9, Capacity = 1, Cost = 11000, Type = TypesRoom[2], HotelID = 2},
+            new(){ID = 10, Capacity = 4, Cost = 12000, Type = TypesRoom[0], HotelID = 2},
+            new(){ID = 11, Capacity = 2, Cost = 13000, Type = TypesRoom[1], HotelID = 2},
+            new(){ID = 12, Capacity = 1, Cost = 14000, Type = TypesRoom[2], HotelID = 2},
+            new(){ID = 13, Capacity = 3, Cost = 15000, Type = TypesRoom[0], HotelID = 3},
+            new(){ID = 14, Capacity = 2, Cost = 16000, Type = TypesRoom[1], HotelID = 3},
+            new(){ID = 15, Capacity = 5, Cost = 17000, Type = TypesRoom[2], HotelID = 3},
+            new(){ID = 16, Capacity = 1, Cost = 18000, Type = TypesRoom[0], HotelID = 3},
+            new(){ID = 17, Capacity = 4, Cost = 19000, Type = TypesRoom[1], HotelID = 4},
+            new(){ID = 18, Capacity = 3, Cost = 20000, Type = TypesRoom[2], HotelID = 4},
+            new(){ID = 19, Capacity = 2, Cost = 21000, Type = TypesRoom[0], HotelID = 4},
+            new(){ID = 20, Capacity = 1, Cost = 22000, Type = TypesRoom[1], HotelID = 4},
+            new(){ID = 21, Capacity = 5, Cost = 23000, Type = TypesRoom[2], HotelID = 5},
         ];
 
-        ArmoredRooms = [
-            new() { 
-                Client = Clients[0], 
-                Room = Rooms[0],
-                DateArrival = new DateOnly(2024, 4, 20),
-                DateEvection = new DateOnly(2024, 5, 10),
-                Period = 30
-            },
-            new() { 
-                Client = Clients[1], 
-                Room= Rooms[1], 
-                DateArrival= new DateOnly(2024, 12, 28),
-                DateEvection = new DateOnly(2025, 1, 5),
-                Period= 8
-            },
-            new() { 
-                Client = Clients[2], 
-                Room= Rooms[2],
-                DateArrival= new DateOnly(2024, 5, 8),
-                DateEvection = new DateOnly(2024, 5, 10),
-                Period= 2
-            },
-            new() { 
-                Client = Clients[3], 
-                Room= Rooms[3],
-                DateArrival= new DateOnly(2024, 6, 1),
-                DateEvection = new DateOnly(2024, 6, 15),
-                Period= 14
-            },
-            new() {
-                Client = Clients[4], 
-                Room= Rooms[4],
-                DateArrival= new DateOnly(2024, 8, 1),
-                DateEvection = new DateOnly(2024, 8, 30),
-                Period= 29
-            }
+        Hotels = [
+            new(){ID = 0, Name = "Гостиница Москва", Address = "Новосадовая 34Э", City = "Вашингтон"},
+            new(){ID = 1, Name = "Grand Hotel", Address = "Park Avenue 123", City = "New York"},
+            new(){ID = 2, Name = "Hilton", Address = "Main Street 55", City = "Chicago"},
+            new(){ID = 3, Name = "Marriott", Address = "Sunset Boulevard 87", City = "Los Angeles"},
+            new(){ID = 4, Name = "Hyatt Regency", Address = "Lake Shore Drive 100", City = "Chicago"},
+            new(){ID = 5, Name = "Ritz-Carlton", Address = "Michigan Avenue 160", City = "Chicago"},
+        ];
+
+       ArmoredRooms = [
+            new(){ Client = Clients[0], DateArrival = new DateOnly(2024, 4, 28), Period = 10, Room = Rooms[0], DateEvection =  new DateOnly(2024, 5, 8)},
+            new(){ Client = Clients[11], DateArrival = new DateOnly(2024, 4, 28), Period= 10, Room = Rooms[11], DateEvection =  new DateOnly(2024, 05, 8)},
+            new(){ Client = Clients[1], DateArrival = new DateOnly(2024, 5, 5), Period= 5, Room = Rooms[1], DateEvection =  new DateOnly(2024, 5, 10)},
+            new(){ Client = Clients[2], DateArrival = new DateOnly(2024, 5, 15), Period= 7, Room= Rooms[2], DateEvection =  new DateOnly(2024, 5, 22)},
+            new(){ Client = Clients[3], DateArrival = new DateOnly(2024, 6, 1), Period= 3, Room= Rooms[3], DateEvection =  new DateOnly(2024, 6, 4)},
+            new(){ Client = Clients[4], DateArrival = new DateOnly(2024, 6, 10), Period= 14, Room= Rooms[4]},
+            new(){ Client = Clients[5], DateArrival = new DateOnly(2024, 6, 20), Period= 9, Room= Rooms[5], DateEvection =  new DateOnly(2024, 06, 29)},
+            new(){ Client = Clients[6], DateArrival = new DateOnly(2024, 7, 1), Period= 6, Room= Rooms[6], DateEvection =  new DateOnly(2024, 7, 7)},
+            new(){ Client = Clients[7], DateArrival = new DateOnly(2024, 7, 10), Period= 11, Room= Rooms[7], DateEvection =  new DateOnly(2024, 7, 21)},
+            new(){ Client = Clients[8], DateArrival = new DateOnly(2024, 7, 20), Period= 8, Room= Rooms[8], DateEvection =  new DateOnly(2024, 7, 28)},
+            new(){ Client = Clients[9], DateArrival = new DateOnly(2024, 8, 1), Period= 4, Room= Rooms[9], DateEvection =  new DateOnly(2024, 8, 5)},
+            new(){ Client = Clients[10], DateArrival = new DateOnly(2024, 8, 10), Period= 12, Room= Rooms[10], DateEvection =  new DateOnly(2024, 8, 22)},
+            new(){ Client = Clients[12], DateArrival = new DateOnly(2024, 6, 1), Period= 3, Room= Rooms[14], DateEvection =  new DateOnly(2024, 6, 4)},
+            new(){ Client = Clients[13], DateArrival = new DateOnly(2024, 6 ,10), Period = 14, Room= Rooms[15], DateEvection =  new DateOnly(2024, 6, 24)},
+            new(){ Client = Clients[14], DateArrival = new DateOnly(2024, 6, 20), Period = 9, Room= Rooms[20], DateEvection =  new DateOnly(2024, 6, 29)},
+            new(){ Client = Clients[15], DateArrival = new DateOnly(2024 ,7, 1), Period = 6, Room= Rooms[16],  DateEvection =  new DateOnly(2024, 7, 7)}
         ];
     }
 }
