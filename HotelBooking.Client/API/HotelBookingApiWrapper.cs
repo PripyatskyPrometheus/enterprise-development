6a94f48ac7ff27c3aa28d4a9778f1cfd867697f5
Chelaev Petr <petrcelaev@gmail.com>
2024-12-20 19:54:22 +0400

    Добавил ещё одну вкладочкупо вашим запросам

--- HotelBooking.Client/API/HotelBookingApiWrapper.cs
+++ HotelBooking.Client/API/HotelBookingApiWrapper.cs
@@ -13,4 +13,34 @@ public class HotelBookingApiWrapper(IConfiguration configuration) : IHotelBookin
     public async Task UpdateHotel(int id, HotelDto hotelDto) => await client.HotelPUTAsync(id, hotelDto);
 
     public async Task DeleteHotel(int id) => await client.HotelDELETEAsync(id);
+
+    public async Task CreateBookedRoom(BookedRoomDto bookedRoomDto) => await client.BookedRoomPOSTAsync(bookedRoomDto);
+
+    public async Task<ICollection<BookedRoomGetDto>> GetAllBookedRooms() => await client.BookedRoomAllAsync();
+
+    public async Task<BookedRoomGetDto> GetBookedRoomById(int id) => await client.BookedRoomGETAsync(id);
+
+    public async Task UpdateBookedRoom(int id, BookedRoomDto bookedRoomDto) => await client.BookedRoomPUTAsync(id, bookedRoomDto);
+
+    public async Task DeleteBookedRoom(int id) => await client.BookedRoomDELETEAsync(id);
+
+    public async Task CreateRoom(RoomDto roomDto) => await client.RoomPOSTAsync(roomDto);
+
+    public async Task<ICollection<RoomGetDto>> GetAllRooms() => await client.RoomAllAsync();
+
+    public async Task<RoomGetDto> GetRoomById(int id) => await client.RoomGETAsync(id);
+
+    public async Task UpdateRoom(int id, RoomDto roomDto) => await client.RoomPUTAsync(id, roomDto);
+
+    public async Task DeleteRoom(int id) => await client.RoomDELETEAsync(id);
+
+    public async Task CreateClient(ClientDto clientDto) => await client.ClientPOSTAsync(clientDto);
+
+    public async Task<ICollection<ClientGetDto>> GetAllClients() => await client.ClientAllAsync();
+
+    public async Task<ClientGetDto> GetClientById(int id) => await client.ClientGETAsync(id);
+
+    public async Task UpdateClient(int id, ClientDto clientDto) => await client.ClientPUTAsync(id, clientDto);
+
+    public async Task DeleteClient(int id) => await client.ClientDELETEAsync(id);
 }
--- HotelBooking.Client/API/IHotelBookingApiWrapper.cs
+++ HotelBooking.Client/API/IHotelBookingApiWrapper.cs
@@ -7,4 +7,34 @@ public interface IHotelBookingApiWrapper
     Task<HotelGetDto> GetHotelById(int id);
     Task UpdateHotel(int id, HotelDto hotelDto);
     Task DeleteHotel(int id);
+
+    Task CreateBookedRoom(BookedRoomDto bookedRoomDto);
+
+    Task<ICollection<BookedRoomGetDto>> GetAllBookedRooms();
+
+    Task<BookedRoomGetDto> GetBookedRoomById(int id);
+
+    Task UpdateBookedRoom(int id, BookedRoomDto bookedRoomDto);
+
+    Task DeleteBookedRoom(int id);
+
+    Task CreateRoom(RoomDto roomDto);
+
+    Task<ICollection<RoomGetDto>> GetAllRooms();
+
+    Task<RoomGetDto> GetRoomById(int id);
+
+    Task UpdateRoom(int id, RoomDto roomDto);
+
+    Task DeleteRoom(int id);
+
+    Task CreateClient(ClientDto clientDto);
+
+    Task<ICollection<ClientGetDto>> GetAllClients();
+
+    Task<ClientGetDto> GetClientById(int id);
+
+    Task UpdateClient(int id, ClientDto clientDto);
+
+    Task DeleteClient(int id);
 }
